319576b484d61672a76a94207e6624d5e6e18009
Tristan Fontanière <tristan.fontaniere@etu.univ-grenoble-alpes.fr>
2024-12-06 16:18:01 +0100

    Modification du fichier de test de faisabilité

--- testbd/simulJeu.php
+++ testbd/simulJeu.php
@@ -5,13 +5,13 @@ ini_set('display_errors', 1);
 $imgPath = './public/img/';
 
 //Connexion à la BDD
-$db = pg_connect("host=192.168.14.118 port=5432 dbname=rezisten user=adminrezisten password=20$S3*01ReZist4n$24");
+$db = pg_connect("host=192.168.14.118 port=5432 dbname=rezisten user=admin_rezisten password=20\$S3*01ReZist4n$24");
 
-$query_titre = 'SELECT titre FROM HISTOIRES WHERE id=1';
+$query_titre = 'SELECT titre FROM HISTOIRES WHERE id_histoire=1';
 $resTitre = pg_query($db,$query_titre);
 $titre = pg_fetch_assoc($resTitre);
 
-$query_persos = 'SELECT nom,prenom,img FROM PERSONNAGES p, APPARITION a WHERE id_histoire=1 AND id_perso=p.id';
+$query_persos = 'SELECT nom,prenom,img FROM PERSONNAGES p, APPARITION a WHERE id_histoire=1 AND a.id_perso=p.id_perso';
 $resPersos = pg_query($db,$query_persos);
 $persos = pg_fetch_all($resPersos);
 
@@ -35,7 +35,7 @@ $dialogue = pg_fetch_all($resDial);
     <main>
     <section id="persos">
         <?php foreach($persos as $personnage) :  ?>
-            <img src="<?= $imgPath.$personnage['img'].'.webp'?>" alt="">
+            <img src="<?= $imgPath.$personnage['img']?>" alt="">
             <?php endforeach; ?>
     </section>
     <div id="dial">
