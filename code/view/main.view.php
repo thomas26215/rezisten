1025c22ea6f5e6e93896294dbfa44b553ff6c835
ARNAUD Sophie <arnaudso@hostname.iut2.upmf-grenoble.fr>
2025-01-06 08:54:40 +0100

    déplacement du dossier design

--- code/view/main.view.php
+++ code/view/main.view.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <title>Ajout d'un nouvel article - Bricomachin</title>
-    <meta name="author" content="Jean-Pierre Chevallet" />
-    <link rel="stylesheet" type="text/css" href="public/design/style.css">
-</head>
-<body>
-    <p>Page principale</p>
-    <p>Prenom : <?= $prenom ?></p>
-    <p>Nombre : <?= $nombre ?></p>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <title>Rézisten</title>
+        <meta name="author" content="Jean-Pierre Chevallet" />
+        <link rel="stylesheet" type="text/css" href="../design/footer.css">
+    </head>
+    <body>
+        <p>Page principale</p>
+        <p>Prenom : <?= $prenom ?></p>
+        <p>Nombre : <?= $nombre ?></p>
+    </body>
+    <footer>
+        <article>Crédit : Freepik Personnages</article>
+        <article><a href="https://www.google.com/">Condition générales d'utilisation</a></article>
+        <article><a href="https://www.google.com/">Partenaire</a></article>
+        <article><a href="https://www.google.com/">Contactez-nous</a></article>
+    </footer>
+</html>
