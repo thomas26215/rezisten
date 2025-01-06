3ff7fca9711fafa3d681f2eb4ec7af5f97a082f3
ARNAUD Sophie <arnaudso@hostname.iut2.upmf-grenoble.fr>
2025-01-06 10:19:09 +0100

    Modification et Terminaison : View->Design : terminaison du footer ;

--- code/view/main.view.php
+++ code/view/main.view.php
@@ -16,11 +16,11 @@
         <article>
             <section>Crédit : 
             <ul> 
-                <li>Freepik</li>
-                <li>Personnages</li>
+                <li><a href="https://fr.freepik.com/">Freepik</a></li>
+                <li><a href="https://charactercreator.org/">Personnages</a></li>
             </ul>
             </section>
-            <section><a href="https://www.google.com/">Condition générales d'utilisation</a></section>
+            <section><a href="https://www.google.com/">Condition générales d'utilisation</a></section> <!-- TO DO : changer les liens pour les bonnes pages-->
             <section><a href="https://www.google.com/">Partenaire</a></section>
             <section><a href="https://www.google.com/">Contactez-nous</a></section>
         </article>
