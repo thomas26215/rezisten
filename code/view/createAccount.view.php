f01eac9c017fd4205baca3cb530e70d0777e7783
CELUSNIAK DE SOUZA Maria <celusnim@hostname.iut2.upmf-grenoble.fr>
2025-01-06 11:54:03 +0100

    modif createAccount

--- code/view/createAccount.view.php
+++ code/view/createAccount.view.php
@@ -5,40 +5,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="design/createAccount.css">
+    <link rel="stylesheet" href="design/footer.css">
 </head>
 <body>
+    <?php include_once("headerHistoire.view.php")?>
+    <div class="body">
+    
     <h1>Créer mon compte</h1>
 
     <form action="post">
-        <label for="pseudo">Pseudonyme <span>*</span> : </label>
-        <input type="text" placeholder="Jano" name="pseudo" id="pseudo">
+        <div class="form">
+            <label for="pseudo">Pseudonyme <span>*</span> : </label>
+            <input type="text" placeholder="Jano" name="pseudo" id="pseudo">
 
-        <label for="nom">Nom : </label>
-        <input type="text" name="nom" id="nom" placeholder="Jean">
+            <label for="nom">Nom : </label>
+            <input type="text" name="nom" id="nom" placeholder="Jean">
 
-        <label for="prenom">Prénom : </label>
-        <input type="text" name="prenom" id="prenom" placeholder="Gaillard">
+            <label for="prenom">Prénom : </label>
+            <input type="text" name="prenom" id="prenom" placeholder="Gaillard">
 
-        <label for="date">Date de naissance <span>*</span> : </label>
-        <input type="date" name="date" id="date">
-        
-        <label for="mail">Adresse mail <span>*</span> : </label>
-        <input type="email" name="mail" id="mail">
+            <label for="date">Date de naissance <span>*</span> : </label>
+            <input type="date" name="date" id="date">
+            
+            <label for="mail">Adresse mail <span>*</span> : </label>
+            <input type="email" name="mail" id="mail">
 
-        <label for="password">Mot de passe <span>*</span> : </label>
-        <input type="password" name="password" id="password">
+            <label for="password">Mot de passe <span>*</span> : </label>
+            <input type="password" name="password" id="password">
 
-        <label for="confirmpass">Confirmer votre mot de passe <span>*</span> : </label>
-        <input type="password" name="confirmpass" id="confirmpass">
+            <label for="confirmpass">Confirmer votre mot de passe <span>*</span> : </label>
+            <input type="password" name="confirmpass" id="confirmpass">
 
-        <input type="checkbox" name="check" id="check">
-        <label for="check">En cochant cette case, j'ai lu et accepte les <a href="">Conditions Générales d'Utilisation</a></label>
+            <input type="checkbox" name="check" id="check">
+            <label for="check">En cochant cette case, j'ai lu et accepte les <a href="">Conditions Générales d'Utilisation</a></label>
 
+        </div>
         <button type="submit">Créer mon compte</button>
         <a href="login.view.php">
             <button>Se connecter</button>
         </a>
     </form>
+    </div>
 
+    <?php include_once("footer.view.php")?>
 </body>
 </html>
