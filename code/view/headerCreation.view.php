ef492ecdc74aa9f12ee80dba49b651c847b809a7
ARNAUD Sophie <arnaudso@hostname.iut2.upmf-grenoble.fr>
2025-01-07 15:50:47 +0100

    Modification : view-> headerCreation

--- code/view/headerCreation.view.php
+++ code/view/headerCreation.view.php
@@ -46,7 +46,7 @@
                 <div>
                     <h2 class="titre"> Ajouter un dialogue </h2>
                 </div>
-                Lorem ipsum odor amet, consectetuer adipiscing elit. Ridiculus sit nisl laoreet facilisis aliquet. Potenti dignissim litora eget montes rhoncus sapien neque urna. Cursus libero sapien integer magnis ligula lobortis quam ut.
+                Lorem ipsum odor amet, consectetuer adipiscing elit. Ridic
             </article>
 
             <section class="footer">
