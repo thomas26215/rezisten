38b40c82797bc2f71f34731465994ade6809db8e
CELUSNIAK DE SOUZA Maria <celusnim@hostname.iut2.upmf-grenoble.fr>
2025-01-07 08:34:56 +0100

    Création et modification : profil.view

--- code/view/main.view.php
+++ code/view/main.view.php
@@ -1,18 +1,5 @@
 <!DOCTYPE html>
 <html lang="fr">
-<<<<<<< HEAD
-<head>
-    <meta charset="utf-8">
-    <title>Ajout d'un nouvel article - Bricomachin</title>
-    <meta name="author" content="Jean-Pierre Chevallet" />
-    <link rel="stylesheet" type="text/css" href="public/design/style.css">
-</head>
-<body>
-    <p>Page principale</p>
-    <p>Prenom : <?= $prenom ?></p>
-    <p>Date : <?= $date ?></p>
-</body>
-=======
     <head>
         <meta charset="utf-8">
         <title>Rézisten</title>
@@ -25,6 +12,5 @@
         <p>Nombre : <?= $nombre ?></p>
     </body>
 
-    <?php include_once("contact.view.php"); ?>
+    <?php include_once("profil.view.php"); ?>
 </html>
->>>>>>> 9e8e577d03f647e7eebb1e13957b441c99b418f2
