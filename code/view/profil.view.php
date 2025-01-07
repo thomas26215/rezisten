65ad06057c5fbfebbe03980e0ed66736eec15aae
QuentinPeg <quentin.peguin@gmail.com>
2025-01-07 11:37:44 +0100

    Modification : Langue des pages html en fr et non en

--- code/view/profil.view.php
+++ code/view/profil.view.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Profil</title>
     <link rel="stylesheet" href="design/profil.css">
 </head>
 
