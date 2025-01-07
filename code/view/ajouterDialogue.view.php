ceea22c48c2a459d8528654bf4a4859ae6492e30
QuentinPeg <quentin.peguin@gmail.com>
2025-01-07 15:45:15 +0100

    mini modif

---
+++ code/view/ajouterDialogue.view.php
@@ -0,0 +1,8 @@
+<article class="content">
+    <div>
+        <h2 class="titre"> Ajouter un dialogue </h2>
+    </div>
+    Lorem ipsum odor amet, consectetuer adipiscing elit. Ridiculus sit nisl laoreet facilisis aliquet.
+    Potenti dignissim litora eget montes rhoncus sapien neque urna. Cursus libero sapien integer magnis
+    ligula lobortis quam ut.
+</article>
