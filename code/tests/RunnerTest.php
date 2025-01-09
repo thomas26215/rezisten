f221b3dcd0b73130cad62fc7a7549dc54a9ac823
Tristan Fontanière <tristan.fontaniere@etu.univ-grenoble-alpes.fr>
2025-01-09 08:21:53 +0100

    Modification : test : ajout des dialogues au runner et ajout de la suppresion des valeurs de test

--- code/tests/RunnerTest.php
+++ code/tests/RunnerTest.php
@@ -10,13 +10,13 @@ include_once(__DIR__. '/chapitreTest.php');
 include_once(__DIR__. '/demandesTest.php');
  */
 
-print("\ntest des lieux : \n\n");
+print("\ntest des Lieux : \n");
 include_once(__DIR__. '/lieuxTest.php');
 
 print("\ntest des Personnages : \n");
 include_once(__DIR__. '/personnagesTest.php');
 
-print("\ntest des Histoires : \n\n");
+print("\ntest des Histoires : \n");
 include_once(__DIR__. '/histoireTest.php');
 
 
@@ -27,6 +27,6 @@ print("\ntest des Dialogues : \n");
 include_once(__DIR__.'/dialoguesTest.php');
 
 print("\ntest des Apparitions : \n");
-include_once(__DIR__.'/apparitionsTest.php');
+include_once(__DIR__.'/apparitionTest.php');
 
 ?>
