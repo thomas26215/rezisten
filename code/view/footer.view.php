edefef2ba08fb3071e4d76400b4e9b1782bb2525
CELUSNIAK DE SOUZA Maria <celusnim@hostname.iut2.upmf-grenoble.fr>
2025-01-10 11:18:20 +0100

    RECTIFICATION FOOTER

--- code/view/footer.view.php
+++ code/view/footer.view.php
@@ -7,9 +7,9 @@
 
 <footer class="flex-row">
     <a id="first-a"href="./main.view.php">
-        <img class="img" src="./design/image/logoCoupe.png" alt="logo Rézisten">
+        <img class="img" src="./view/design/image/logoCoupe.png" alt="logo Rézisten">
     </a>
-    <article>
+    <article class="flex-row">
         <section>Crédit :
             <ul>
                 <li><a href="https://fr.freepik.com/" target="_blank">Freepik</a></li>
