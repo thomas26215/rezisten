8692d66945074c81acccad93ee79aaa5fee85082
JOANNIC Elouan <joannice@hostname.iut2.upmf-grenoble.fr>
2025-01-13 11:07:54 +0100

    Creation code->controleur demandeCreateur

---
+++ code/controler/demandeCreateur.ctrl.php
@@ -0,0 +1,14 @@
+<?php
+include_once('./model/users.class.php');
+include_once('framework/view.fw.php');
+
+$user = User::read(1);
+
+$nom = $user->getSurname();
+$prenom = $user->getFirstName();
+
+$view = new View();
+$view->assign('nom', $nom);
+$view->assign('prenom', $prenom);
+$view->display('demandeCreateur');
+?>
