c18b68362badf10fee6ae6f8df2b8237ed23bd61
fontanit <tristan.fontaniere@etu.univ-grenoble-alpes.fr>
2025-01-14 09:33:05 +0100

    Modification : main : prise en compte du role user

--- code/controler/main.ctrl.php
+++ code/controler/main.ctrl.php
@@ -4,6 +4,9 @@ include_once('./framework/view.fw.php');
 include_once('./model/users.class.php');
 include_once('./model/users.class.php');
 
+$user = User::read($_SESSION["user_id"]);
+
+$userrole = $user->getRole();
 
 
 
