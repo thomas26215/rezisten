3f5dbf3a3aa95bef8e18526c3f6ce9e1e4b66be2
fontanit <tristan.fontaniere@etu.univ-grenoble-alpes.fr>
2025-01-14 14:45:06 +0100

    Commentaire : controler->histoire : explication gestion fin

--- code/controler/histoire.ctrl.php
+++ code/controler/histoire.ctrl.php
@@ -29,9 +29,8 @@ $dialogPrev = Dialog::read($idDialog - 1, $idStory);
 
 
 if ($dialog === null) {
-    // S'il n'y a plus de dialogue => fin avec bonus, on met
+    // S'il n'y a plus de dialogue => fin avec bonus, on met à jour la progression si besoin en vérifiant qu'on est bien sur une fin bonus
     if ($idDialog >= Dialog::readFirstBonus($idStory)) {
-        // Cas de la fin bonus
         if (!Progression::read($_SESSION['user_id'], $_SESSION['idStory'] + 1)) {
             $progression = new Progression(
                 User::read($_SESSION['user_id']),
@@ -47,7 +46,8 @@ if ($dialog === null) {
     $view->assign('place', $place);
     $view->display('finHistoire');
 } elseif($dialogPrev != null && $dialog->getBonus() != $dialogPrev->getBonus() && $_SESSION['difficulty'] == "générique") {
-    // Vérifications supplémentaires si le dialogue existe
+    // Sinon il faut s'assurer qu'on est pas dans le cas du dialogue numéro 1, que le dialogue demandé est  bonus sachant que le dialogue d'avant est non bonus
+    // et que la difficulté est bien celle générique, ensuite on réalise les mêmes opérations concernant la progression
 
             if (!Progression::read($_SESSION['user_id'], $_SESSION['idStory'] + 1)) {
                 $progression = new Progression(
