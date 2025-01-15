d2f75adcad935437e6187a8f24fbdf99b154bf9a
JOANNIC Elouan <joannice@hostname.iut2.upmf-grenoble.fr>
2025-01-15 11:46:37 +0100

    Modification view + controleur profil -> message d'erreur si changement d'email avec mauvais format

--- code/controler/profil.ctrl.php
+++ code/controler/profil.ctrl.php
@@ -3,6 +3,7 @@ include_once('./model/users.class.php');
 include_once('framework/view.fw.php');
 
 
+$errors = [];
 $user = User::read((int)$_SESSION['user_id']);
 
 
@@ -12,8 +13,13 @@ if (isset($_POST['pseudo']) && strlen($_POST['pseudo'])!=0) {
     $user->update();
 }
 if (isset($_POST['mail']) && strlen($_POST['mail'])!=0) {
-    $user->setMail($_POST['mail']);
-    $user->update();
+    if (filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL) && (strpos($_POST['mail'], '.com') || strpos($_POST['mail'], '.fr')) !== false) {
+        $user->setMail($_POST['mail']);
+        $user->update();
+    }
+    else {
+        $errors[] = "Email au mauvais format";
+    }
 }
 
 
@@ -31,5 +37,6 @@ $mail = $user->getMail();
 $view = new View();
 $view->assign('pseudo', $pseudo);
 $view->assign('mail', $mail);
+$view->assign('errors', $errors);
 $view->display('profil');
 ?>
