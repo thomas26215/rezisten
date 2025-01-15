268f8b9fc6ff8e1e0e19a52f0bcbb8ede70258af
celusnim <malu.celusniak@gmail.com>
2025-01-15 14:47:53 +0100

    modification : personnages.ctrl methodes terminés

--- code/model/personnages.class.php
+++ code/model/personnages.class.php
@@ -3,14 +3,16 @@
 require_once(__DIR__ . "/dao.class.php");
 require_once(__DIR__ . "/users.class.php");
 
-class Character {
+class Character
+{
     private int $id;
     private string $first_name;
     private string $image;
     private User $creator;
     private DAO $dao;
 
-    public function __construct(string $first_name, string $image, User $creator, int $id = -1) {
+    public function __construct(string $first_name, string $image, User $creator, int $id = -1)
+    {
         $this->setId($id);
         $this->setFirstName($first_name);
         $this->setImage($image);
@@ -19,24 +21,29 @@ class Character {
     }
 
     // Getters
-    public function getId(): int {
+    public function getId(): int
+    {
         return $this->id;
     }
 
-    public function getFirstName(): string {
+    public function getFirstName(): string
+    {
         return $this->first_name;
     }
 
-    public function getImage(): string {
+    public function getImage(): string
+    {
         return $this->image;
     }
 
-    public function getCreator() : User {
+    public function getCreator(): User
+    {
         return $this->creator;
     }
 
     // Setters
-    public function setId(int $id): void {
+    public function setId(int $id): void
+    {
         if ($id >= -1) {
             $this->id = $id;
         } else {
@@ -44,22 +51,25 @@ class Character {
         }
     }
 
-    public function setFirstName(string $first_name): void {
-        if(empty($first_name)) {
+    public function setFirstName(string $first_name): void
+    {
+        if (empty($first_name)) {
             throw new Exception("Le prénom ne peut pas être vide");
         }
         $this->first_name = $first_name;
     }
 
-    public function setImage(string $image): void {
+    public function setImage(string $image): void
+    {
         if (empty($image)) {
             throw new Exception("L'image ne peut pas être vide.");
         }
         $this->image = $image;
     }
 
-    public function setCreator(User $creator) : void {
-        if(empty($creator)){
+    public function setCreator(User $creator): void
+    {
+        if (empty($creator)) {
             throw new Exception("Le créateurne peut pas être vide");
         }
         $this->creator = $creator;
@@ -68,21 +78,25 @@ class Character {
 
     /* --- Méthodes CRUD --- */
 
-    public function create(): bool {
-        if($this->dao->insertRelatedData("personnages", [
-            "prenom" => $this->first_name,
-            "img" => $this->image,
-            "createur" => $this->creator->getId()
-        ])){
+    public function create(): bool
+    {
+        if (
+            $this->dao->insertRelatedData("personnages", [
+                "prenom" => $this->first_name,
+                "img" => $this->image,
+                "createur" => $this->creator->getId()
+            ])
+        ) {
             $this->setId($this->dao->getLastInsertId("personnages")[0]["last_id"]);
             return true;
         }
         return false;
     }
 
-    public static function read(int $id): ?Character {
+    public static function read(int $id): ?Character
+    {
         $dao = DAO::getInstance();
-        if($characterData = $dao->getColumnWithParameters("personnages", ["id" => $id])){
+        if ($characterData = $dao->getColumnWithParameters("personnages", ["id" => $id])) {
             $characterDatas = $characterData[0];
             return new Character(
                 $characterDatas["prenom"],
@@ -94,58 +108,62 @@ class Character {
         return null;
     }
 
-    public static function readCharactersOfUser(int $userId) : array {
+    public static function readCharactersOfUser(int $userId): array
+    {
         $dao = DAO::getInstance();
         $listCharacters = array();
-        
-        $characters = $dao->getColumnWithParameters("personnages",["createur" => $userId]);
-        if(empty($characters)){
+
+        $characters = $dao->getColumnWithParameters("personnages", ["createur" => $userId]);
+        if (empty($characters)) {
             throw new Exception("aucun personnage trouvé");
         }
 
-        for($i = 0 ; $i < sizeof() ; $i++){
+        for ($i = 0; $i < sizeof(); $i++) {
             $creat = User::read($characters[$i]['createur']);
-            $c = new Character($characters[$i]["prenom"],$characters[$i]["img"],$creat,$characters[$i]["id"]);
-            array_push($listCharacters,$c);
+            $c = new Character($characters[$i]["prenom"], $characters[$i]["img"], $creat, $characters[$i]["id"]);
+            array_push($listCharacters, $c);
         }
         return $listCharacters;
     }
 
-    public function update(): bool {
-        if($this->id !== -1){
+    public function update(): bool
+    {
+        if ($this->id !== -1) {
             return $this->dao->update("personnages", [
                 "prenom" => $this->first_name,
                 "img" => $this->image,
-                "createur" => $this->creator,
-            ], ["id" => (int)$this->id]) > 0;
+                "createur" => $this->creator->getId(),
+            ], ["id" => (int) $this->id]) > 0;
         }
+        return false;
     }
 
-    public static function delete($id): bool {
-        if($id > 0){
+    public static function delete($id): bool
+    {
+        if ($id > 0) {
             return DAO::getInstance()->deleteDatasById("personnages", $id);
         }
         return false;
     }
 
-    public static function readAllCharacters() : array{
+    public static function readAllCharacters(): array
+    {
         $dao = DAO::getInstance();
         $listCharacters = array();
-        $characters = $dao->getColumnWithParameters("personnages",[]);
-        if(empty($characters)){
+        $characters = $dao->getColumnWithParameters("personnages", []);
+        if (empty($characters)) {
             throw new Exception("aucun personnage trouvé");
         }
 
-        for($i = 0; $i < sizeof($characters); $i++){
+        for ($i = 0; $i < sizeof($characters); $i++) {
             $creat = User::read($characters[$i]['createur']);
-           
-            $c = new Character($characters[$i]['prenom'],$characters[$i]['img'],$creat,$characters[$i]['id']);
-            array_push($listCharacters,$c);
+
+            $c = new Character($characters[$i]['prenom'], $characters[$i]['img'], $creat, $characters[$i]['id']);
+            array_push($listCharacters, $c);
         }
 
         return $listCharacters;
-        
+
     }
 }
 ?>
-
