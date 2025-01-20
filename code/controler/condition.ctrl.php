774ae165165846ba0cd01262339f4f103755d2f6
root <root@debian>
2025-01-20 14:52:55 +0100

    Modification : ajout des derniers controleurs

---
+++ code/controler/condition.ctrl.php
@@ -0,0 +1,7 @@
+<?php
+include_once('framework/view.fw.php');
+
+$view = new View();
+$view->display('conditionGeneralesDutilisation');
+
+?>
---
+++ code/controler/partenaire.ctrl.php
@@ -0,0 +1,6 @@
+<?php
+include_once('framework/view.fw.php');
+
+$view = new View();
+$view->display('partenaire');
+?>
