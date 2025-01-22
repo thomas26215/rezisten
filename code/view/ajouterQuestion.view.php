4da6a188a203a12db981a6528de3024ec49a1e89
celusnim <malu.celusniak@gmail.com>
2025-01-22 09:49:10 +0100

    modification : page création

--- code/view/ajouterQuestion.view.php
+++ code/view/ajouterQuestion.view.php
@@ -9,6 +9,11 @@
         <input type="hidden" name="article" value="ajouterQuestion">
         <input type="hidden" name="id" value="<?= $histoire->getId() ?>">
 
+        <div class="flex-col texte">
+            <span>Attention !</span>
+            <p>Vous ne pouvez rajouter qu'une question dans votre histoire. Une deuxième question rajoutée remplacera la première.</p>
+        </div>
+
         <!-- Question -->
         <section class="input-superpose">
             <label for="question">Entrez la question : </label>
