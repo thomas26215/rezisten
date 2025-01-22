c1001010286afc2b2a70417e1f4135d0d46fe253
root <root@debian>
2025-01-22 14:43:25 +0100

    Modification changement de mdp

--- code/view/changermotdepasse.view.php
+++ code/view/changermotdepasse.view.php
@@ -56,7 +56,7 @@
 
             <div class="buttons">
                 <button type="submit">Changer mot de passe</button>
-                <?php if ($mode === 'authentified'): ?>
+                <?php if ($mode === 'forget'): ?>
                     <a href="login.view.php">Retour à la page de connexion</a>
                 <?php endif; ?>
             </div>
